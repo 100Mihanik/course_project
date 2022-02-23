c684c80467b143a0be853bec7a8a8542f6070f99
100Mihanik <100Mihanik@mail.ru>
2022-02-23 15:22:33 +0300

    added bootstrap and create header

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import { Col, Row } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Row>
+        <Col md="6">Username</Col>
+        <Col md="6">
+					
+				</Col>
+      </Row>
+    </header>
+  );
+}
+
+export default Header;
